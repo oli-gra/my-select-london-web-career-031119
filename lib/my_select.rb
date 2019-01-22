b904c4902b910edace7e5f5035287ea6c485d34f
Oliver <oliver.grant@gmail.com>
2019-01-22 13:35:38 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,6 +3,8 @@ def my_select(selection)
   my_selection = []
   while i < selection.length
     my_selection[i] = yield selection[i]
+    if my_selection[i] == true
+      my_selection[i] = selection[i]
     i += 1
   end
   my_selection
