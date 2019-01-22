1f32e5d457b7fe5a2ddc7069f62856e98fdf5511
Oliver <oliver.grant@gmail.com>
2019-01-22 13:59:48 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
+def my_select(selection)
+  i = 0
+  my_selection = []
+  while i < selection.length
+    my_selection[i] = yield selection[i] # yields a boolean
+    if my_selection[i] == true # we test truthiness
+      my_selection[i] = selection[i]
+    end
+    i += 1
+  end
+  my_selection - [false]
 end
