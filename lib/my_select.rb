2fde6d3bc44d8966334aaaf6d2eb85eaf2eed31b
Oliver <oliver.grant@gmail.com>
2019-01-22 13:38:33 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,11 +2,11 @@ def my_select(selection)
   i = 0
   my_selection = []
   while i < selection.length
-    my_selection[i] = yield selection[i]
-    if my_selection[i] == true
+    my_selection[i] = yield selection[i] # yields a boolean
+    if my_selection[i] == true # we test truthiness
       my_selection[i] = selection[i]
     end
     i += 1
   end
-  my_selection
+  my_selection.uniq
 end
