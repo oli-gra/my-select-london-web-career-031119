1960dc9caf219d70de0c6f45f34758f2f71a6905
Oliver <oliver.grant@gmail.com>
2019-01-22 13:31:53 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
-def my_select(collection)
- # your code here!
+def my_select(selection)
+  i = 0
+  my_selection = []
+  while i < selection.length
+    case yield selection[i]
+    when selection[i] == false
+      i += 1
+    when selection[i] == true
+      my_selection[i] = selection[i]
+      i += 1
+    else
+      puts "No argument parsed"
+    end
+  end
+  my_selection
 end
