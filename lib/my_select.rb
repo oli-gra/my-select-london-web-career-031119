debf72688f70ca11cc763a59e6eb9363c5f4d5f6
Oliver <oliver.grant@gmail.com>
2019-01-22 13:34:16 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(selection)
   i = 0
   my_selection = []
   while i < selection.length
-    my_selection[i] = case yield selection[i]
+    my_selection[i] = yield selection[i]
     i += 1
   end
   my_selection
